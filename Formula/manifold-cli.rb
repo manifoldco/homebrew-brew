bf256da8138d46ae758aa6b75ee503b2306969ea
James Bowes <jbowes@repl.ca>
2017-08-10 16:32:31 -0300

    Update formula for manifold-cli to v0.1.0

---
+++ Formula/manifold-cli.rb
@@ -0,0 +1,47 @@
+
+require "language/go"
+
+class Manifold-Cli< Formula
+  desc "nothing"
+  homepage "https://www.manifold.co"
+  url "https://github.com/manifoldco/manifold-cli/archive/v0.1.0.tar.gz"
+  sha256 ""
+  head "https://github.com/manifoldco/manifold-cli.git"
+
+  depends_on "glide" => :build
+  depends_on "go" => :build
+
+  bottle do
+    root_url "https://releases.manifold.co/manifold-cli/brew/bottles"
+    cellar :any_skip_relocation
+    sha256 "936c8a99106713603ef897f83b919cbbd04d20546aabaf5ff357b331498f67cc" => :sierra
+  end
+
+  go_resource "github.com/jteeuwen/go-bindata" do
+    url "https://github.com/jteeuwen/go-bindata.git",
+        :revision => "a0ff2567cfb70903282db057e799fd826784d41d"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = buildpath/"glide_home"
+
+    pkgpath = buildpath/"src/github.com/manifoldco/manifold-cli"
+    pkgpath.install Dir["*"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/jteeuwen/go-bindata" do
+      system "go", "install", "github.com/jteeuwen/go-bindata/..."
+    end
+    ENV.prepend_path "PATH", buildpath/"bin"
+
+    cd pkgpath do
+      arch = MacOS.prefer_64_bit? ? "amd64" : "386"
+      ENV.deparallelize do
+        system "make", "binary-darwin-#{arch}", "VERSION=v0.1.0", "BYPASS_GO_CHECK=yes"
+      end
+
+      bin.install "builds/bin/v0.1.0/darwin/#{arch}/manifold-cli"
+    end
+  end
+end
