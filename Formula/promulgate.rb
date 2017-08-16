f727f05a7c45c56285a4a45d4080dfb21cd6539a
manifold-dangerzone <admin+github-bot-dangerzone@manifold.co>
2017-08-16 10:23:27 -0300

    Update formula for promulgate to v0.0.4

--- Formula/promulgate.rb
+++ Formula/promulgate.rb
@@ -4,7 +4,7 @@ require "language/go"
 class Promulgate< Formula
   desc "Manifold's release tool"
   homepage "https://www.manifold.co"
-  url "https://github.com/manifoldco/promulgate/archive/v0.0.1.tar.gz"
+  url "https://github.com/manifoldco/promulgate/archive/v0.0.4.tar.gz"
   sha256 ""
   head "https://github.com/manifoldco/promulgate.git"
 
@@ -14,9 +14,9 @@ class Promulgate< Formula
   bottle do
     root_url "https://releases.manifold.co/promulgate/brew/bottles"
     cellar :any_skip_relocation
-    sha256 "852982f8c47abdff89579ec628afa2acd6edf3d6fa9c2f297f8072f2140ae7ad" => :sierra
-    sha256 "852982f8c47abdff89579ec628afa2acd6edf3d6fa9c2f297f8072f2140ae7ad" => :el_capitan
-    sha256 "852982f8c47abdff89579ec628afa2acd6edf3d6fa9c2f297f8072f2140ae7ad" => :yosemite
+    sha256 "019ab2502ebccb5eccc95be161a7dca0944eeb72820a79314cc7ac5b6ac26019" => :sierra
+    sha256 "019ab2502ebccb5eccc95be161a7dca0944eeb72820a79314cc7ac5b6ac26019" => :el_capitan
+    sha256 "019ab2502ebccb5eccc95be161a7dca0944eeb72820a79314cc7ac5b6ac26019" => :yosemite
   end
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -40,10 +40,10 @@ class Promulgate< Formula
     cd pkgpath do
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV.deparallelize do
-        system "make", "binary-darwin-#{arch}", "VERSION=v0.0.1", "BYPASS_GO_CHECK=yes"
+        system "make", "binary-darwin-#{arch}", "VERSION=v0.0.4", "BYPASS_GO_CHECK=yes"
       end
 
-      bin.install "builds/bin/v0.0.1/darwin/#{arch}/promulgate"
+      bin.install "builds/bin/v0.0.4/darwin/#{arch}/promulgate"
     end
   end
 end
