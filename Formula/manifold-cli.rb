2dffe887dfcdc7709a3b9081997375d4d1f207f3
manifold-dangerzone <admin+github-bot-dangerzone@manifold.co>
2017-08-29 13:59:48 -0300

    Update formula for manifold-cli to v0.3.0

--- Formula/manifold-cli.rb
+++ Formula/manifold-cli.rb
@@ -2,9 +2,9 @@
 require "language/go"
 
 class ManifoldCli< Formula
-  desc "A prototype of a possible Manifold CLI"
-  homepage "https://www.manifold.co"
-  url "https://github.com/manifoldco/manifold-cli/archive/v0.2.6.tar.gz"
+  desc "Manifold CLI"
+  homepage "https://www.manifold.co/cli"
+  url "https://github.com/manifoldco/manifold-cli/archive/v0.3.0.tar.gz"
   sha256 ""
   head "https://github.com/manifoldco/manifold-cli.git"
 
@@ -14,9 +14,9 @@ class ManifoldCli< Formula
   bottle do
     root_url "https://releases.manifold.co/manifold-cli/brew/bottles"
     cellar :any_skip_relocation
-    sha256 "a040336c2614efd847d23acc51aac8fd0163001b3279d0642dd8c04790be8df8" => :sierra
-    sha256 "a040336c2614efd847d23acc51aac8fd0163001b3279d0642dd8c04790be8df8" => :el_capitan
-    sha256 "a040336c2614efd847d23acc51aac8fd0163001b3279d0642dd8c04790be8df8" => :yosemite
+    sha256 "6b2306c0ffb6da62fd6868cba312df215475d08add04fb3eda531e46de3cf27a" => :sierra
+    sha256 "6b2306c0ffb6da62fd6868cba312df215475d08add04fb3eda531e46de3cf27a" => :el_capitan
+    sha256 "6b2306c0ffb6da62fd6868cba312df215475d08add04fb3eda531e46de3cf27a" => :yosemite
   end
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -40,10 +40,10 @@ class ManifoldCli< Formula
     cd pkgpath do
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV.deparallelize do
-        system "make", "binary-darwin-#{arch}", "VERSION=v0.2.6", "BYPASS_GO_CHECK=yes"
+        system "make", "binary-darwin-#{arch}", "VERSION=v0.3.0", "BYPASS_GO_CHECK=yes"
       end
 
-      bin.install "builds/bin/v0.2.6/darwin/#{arch}/manifold"
+      bin.install "builds/bin/v0.3.0/darwin/#{arch}/manifold"
     end
   end
 end
