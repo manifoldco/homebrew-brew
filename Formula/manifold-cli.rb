4e0fa1ba8a6b44dd3f5414ea6800c2e7b5043e70
manifold-dangerzone <admin+github-bot-dangerzone@manifold.co>
2018-04-13 14:11:50 -0300

    Update formula for manifold-cli to v0.14.0

--- Formula/manifold-cli.rb
+++ Formula/manifold-cli.rb
@@ -4,7 +4,7 @@ require "language/go"
 class ManifoldCli< Formula
   desc "Manage your services and config from the command line"
   homepage "https://www.manifold.co/cli"
-  url "https://github.com/manifoldco/manifold-cli/archive/v0.13.0.tar.gz"
+  url "https://github.com/manifoldco/manifold-cli/archive/v0.14.0.tar.gz"
   sha256 ""
   head "https://github.com/manifoldco/manifold-cli.git"
 
@@ -14,10 +14,10 @@ class ManifoldCli< Formula
   bottle do
     root_url "https://releases.manifold.co/manifold-cli/brew/bottles"
     cellar :any_skip_relocation
-    sha256 "a652d82dd84ef13ec0bf6d07f8e2e3d2c4ecfed4b2ad09d3ba58e8a25ec659ba" => :high_sierra
-    sha256 "a652d82dd84ef13ec0bf6d07f8e2e3d2c4ecfed4b2ad09d3ba58e8a25ec659ba" => :sierra
-    sha256 "a652d82dd84ef13ec0bf6d07f8e2e3d2c4ecfed4b2ad09d3ba58e8a25ec659ba" => :el_capitan
-    sha256 "a652d82dd84ef13ec0bf6d07f8e2e3d2c4ecfed4b2ad09d3ba58e8a25ec659ba" => :yosemite
+    sha256 "9dcf56a789b572c520e4bbf1b8605c9bfb2eea1ac3825300de3785320d6eb787" => :high_sierra
+    sha256 "9dcf56a789b572c520e4bbf1b8605c9bfb2eea1ac3825300de3785320d6eb787" => :sierra
+    sha256 "9dcf56a789b572c520e4bbf1b8605c9bfb2eea1ac3825300de3785320d6eb787" => :el_capitan
+    sha256 "9dcf56a789b572c520e4bbf1b8605c9bfb2eea1ac3825300de3785320d6eb787" => :yosemite
   end
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -41,10 +41,10 @@ class ManifoldCli< Formula
     cd pkgpath do
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV.deparallelize do
-        system "make", "binary-darwin-#{arch}", "VERSION=v0.13.0", "BYPASS_GO_CHECK=yes"
+        system "make", "binary-darwin-#{arch}", "VERSION=v0.14.0", "BYPASS_GO_CHECK=yes"
       end
 
-      bin.install "builds/bin/v0.13.0/darwin/#{arch}/manifold"
+      bin.install "builds/bin/v0.14.0/darwin/#{arch}/manifold"
     end
   end
 end
