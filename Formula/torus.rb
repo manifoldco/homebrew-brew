56586fb5cb6b52064317ddf39d36727f07d5f44e
Patrick LaRoche <patrick@manifold.co>
2018-06-29 13:32:39 -0300

    Update torus to v0.30.2

--- Formula/torus.rb
+++ Formula/torus.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Torus < Formula
   desc "A secure, shared workspace for secrets"
   homepage "https://www.torus.sh"
-  url "https://github.com/manifoldco/torus-cli/archive/v0.30.1.tar.gz"
-  sha256 "2c1542c2cf474d4a9a895c2cbabcd00da80d9b66d0de4d71b50c0cd100065323"
+  url "https://github.com/manifoldco/torus-cli/archive/v0.30.2.tar.gz"
+  sha256 "d1ad8e0e7e1f5e1d9f21e0c896b12ae0a5210fd613fab4c4e76958ad11f06127"
   head "https://github.com/manifoldco/torus-cli.git"
 
   depends_on "glide" => :build
@@ -13,10 +13,10 @@ class Torus < Formula
   bottle do
     root_url "https://get.torus.sh/brew/bottles"
     cellar :any_skip_relocation
-    sha256 "47c9cefe06f440fdd40c17d4f59dd301adb1f65b4973b253bf14e482bb9ec37c" => :high_sierra
-    sha256 "47c9cefe06f440fdd40c17d4f59dd301adb1f65b4973b253bf14e482bb9ec37c" => :sierra
-    sha256 "47c9cefe06f440fdd40c17d4f59dd301adb1f65b4973b253bf14e482bb9ec37c" => :el_capitan
-    sha256 "47c9cefe06f440fdd40c17d4f59dd301adb1f65b4973b253bf14e482bb9ec37c" => :yosemite
+    sha256 "2944a3df228cf05d4204f666240bb4a81cf60fd20296fde01d0d0dd1e9ebfa22" => :high_sierra
+    sha256 "2944a3df228cf05d4204f666240bb4a81cf60fd20296fde01d0d0dd1e9ebfa22" => :sierra
+    sha256 "2944a3df228cf05d4204f666240bb4a81cf60fd20296fde01d0d0dd1e9ebfa22" => :el_capitan
+    sha256 "2944a3df228cf05d4204f666240bb4a81cf60fd20296fde01d0d0dd1e9ebfa22" => :yosemite
   end
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -40,10 +40,10 @@ class Torus < Formula
     cd toruspath do
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV.deparallelize do
-        system "make", "binary-darwin-#{arch}", "VERSION=0.30.1", "BYPASS_GO_CHECK=yes"
+        system "make", "binary-darwin-#{arch}", "VERSION=0.30.2", "BYPASS_GO_CHECK=yes"
       end
 
-      bin.install "builds/bin/0.30.1/darwin/#{arch}/torus"
+      bin.install "builds/bin/0.30.2/darwin/#{arch}/torus"
     end
 
     getting_started_url = "https://www.torus.sh/docs/latest/start-here/quickstart"
