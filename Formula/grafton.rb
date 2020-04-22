75b9851b6fc8afea7d057b7c44b9cbf832749516
Domenic Rosati <domenicrosati@gmail.com>
2020-04-22 16:17:48 -0300

    Update formula for grafton to v0.16.2

--- Formula/grafton.rb
+++ Formula/grafton.rb
@@ -4,7 +4,7 @@ require "language/go"
 class Grafton< Formula
   desc "Manifold's provider validation tool"
   homepage "https://www.manifold.co/partners"
-  url "https://github.com/manifoldco/grafton/archive/v0.16.1.tar.gz"
+  url "https://github.com/manifoldco/grafton/archive/v0.16.2.tar.gz"
   sha256 ""
   head "https://github.com/manifoldco/grafton.git"
 
@@ -14,10 +14,10 @@ class Grafton< Formula
   bottle do
     root_url "https://releases.manifold.co/grafton/brew/bottles"
     cellar :any_skip_relocation
-    sha256 "2ad8b04bdbf927f136c14da42ba6bc2e18e5a4673650c1b20cb33061e842198a" => :high_sierra
-    sha256 "2ad8b04bdbf927f136c14da42ba6bc2e18e5a4673650c1b20cb33061e842198a" => :sierra
-    sha256 "2ad8b04bdbf927f136c14da42ba6bc2e18e5a4673650c1b20cb33061e842198a" => :el_capitan
-    sha256 "2ad8b04bdbf927f136c14da42ba6bc2e18e5a4673650c1b20cb33061e842198a" => :yosemite
+    sha256 "29b48185eab08555fb63b80dadb08e89c76682e183e9fbed50bfdb9e5d2ee429" => :high_sierra
+    sha256 "29b48185eab08555fb63b80dadb08e89c76682e183e9fbed50bfdb9e5d2ee429" => :sierra
+    sha256 "29b48185eab08555fb63b80dadb08e89c76682e183e9fbed50bfdb9e5d2ee429" => :el_capitan
+    sha256 "29b48185eab08555fb63b80dadb08e89c76682e183e9fbed50bfdb9e5d2ee429" => :yosemite
   end
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -41,10 +41,10 @@ class Grafton< Formula
     cd pkgpath do
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV.deparallelize do
-        system "make", "binary-darwin-#{arch}", "VERSION=v0.16.1", "BYPASS_GO_CHECK=yes"
+        system "make", "binary-darwin-#{arch}", "VERSION=v0.16.2", "BYPASS_GO_CHECK=yes"
       end
 
-      bin.install "builds/bin/v0.16.1/darwin/#{arch}/grafton"
+      bin.install "builds/bin/v0.16.2/darwin/#{arch}/grafton"
     end
   end
 end
